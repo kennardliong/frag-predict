abbde265b578865b9fb64d6d366133531d262a3c
kennardliong <kennardliong1@gmail.com>
2024-07-27 17:08:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,38 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
         viewer.setStyle({}, {stick: {colorscheme: 'Jmol'}});
         viewer.zoomTo();
         viewer.render();
+
+        const inputDownloadLink = document.getElementById('input-download');
+        inputDownloadLink.style.display = 'block';
+        inputDownloadLink.onclick = function() {
+            fetch('http://localhost:5000/download_pdb', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ smiles: smiles, filename: 'input_structure.pdb' })
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                } else {
+                    alert('Failed to download PDB file');
+                }
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = 'input_structure.pdb';
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        };
     })
     .catch(error => {
         console.error('Error:', error);
@@ -58,8 +90,18 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 
         const fragmentSmiles = data.fragment_smiles;
         const fragmentPdb = data.pdb;
+        const properties = data.properties;
+
         console.log("Best Fragment SMILES:", fragmentSmiles);
-        document.getElementById('fragment-output').textContent = `Best Fragment SMILES: ${fragmentSmiles}`;
+        document.getElementById('fragment-properties').innerHTML = `
+            <p></p>
+            <p><span class="pink-title">Best Fragment SMILES: </span> ${fragmentSmiles}</p>   
+            <p><span class="pink-title">Molecular Weight: </span>${properties.molecular_weight.toFixed(2)}</p>
+            <p><span class="pink-title">LogP: </span>${properties.log_p.toFixed(2)}</p>
+            <p><span class="pink-title">Hydrogen Bond Donors: </span>${properties.hydrogen_bond_donors}</p>
+            <p><span class="pink-title">Hydrogen Bond Acceptors: </span>${properties.hydrogen_bond_acceptors}</p>
+            <p><span class="pink-title">TPSA: </span> ${properties.tpsa.toFixed(2)}</p>
+        `;
 
         const viewerFragmented = $3Dmol.createViewer("viewer-fragmented", {
             defaultcolors: $3Dmol.rasmolElementColors,
@@ -70,6 +112,38 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
         viewerFragmented.setStyle({}, {stick: {colorscheme: 'Jmol'}});
         viewerFragmented.zoomTo();
         viewerFragmented.render();
+
+        const fragmentDownloadLink = document.getElementById('fragment-download');
+        fragmentDownloadLink.style.display = 'block';
+        fragmentDownloadLink.onclick = function() {
+            fetch('http://localhost:5000/download_pdb', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ smiles: fragmentSmiles, filename: 'fragment_structure.pdb' })
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                } else {
+                    alert('Failed to download PDB file');
+                }
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = 'fragment_structure.pdb';
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        };
     })
     .catch(error => {
         console.error('Error:', error);
