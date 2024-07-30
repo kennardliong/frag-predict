5bd52b9a2cec57e8864261eec9b7d938ccd995cd
kennardliong <kennardliong1@gmail.com>
2024-07-30 22:18:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.getElementById('inputForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const smiles = document.getElementById('smilesInput').value;
+
+    const smiles1 = document.getElementById('smilesInput1').value;
+    const smiles2 = document.getElementById('smilesInput2').value;
     const protein = document.getElementById('proteinSelector').value;
 
     if (!protein) {
@@ -8,219 +10,243 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
         return;
     }
 
-    console.log("SMILES String:", smiles);
-    console.log("Selected Protein:", protein);
-
-    // Show sections and buttons
-    document.getElementById('subtitle-inputted').style.display = 'block';
-    document.getElementById('subtitle-fragmented').style.display = 'block';
-    document.getElementById('toggle-input-view').style.display = 'block';
-    document.getElementById('toggle-fragment-view').style.display = 'block';
-
-    // Fetch 2D image for input
-    fetch('http://localhost:5000/get_2d_structure', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ smiles: smiles })
-    })
-    .then(response => response.blob())
-    .then(blob => {
-        const url = window.URL.createObjectURL(blob);
-        document.getElementById('input-2d').src = url;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-    // Fetch 3D structure for input
-    fetch('http://localhost:5000/get_3d_structure', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ smiles: smiles })
-    })
-    .then(response => response.json())
-    .then(data => {
-        const viewer = $3Dmol.createViewer("viewer", {
-            defaultcolors: $3Dmol.rasmolElementColors,
-            backgroundColor: 'black'
-        });
-
-        viewer.addModel(data.pdb, "pdb");
-        viewer.setStyle({}, {stick: {colorscheme: 'Jmol'}});
-        viewer.zoomTo();
-        viewer.render();
-
-        const inputDownloadLink = document.getElementById('input-download');
-        inputDownloadLink.style.display = 'block';
-        inputDownloadLink.onclick = function() {
-            fetch('http://localhost:5000/download_pdb', {
+    async function fetch2DStructure(smiles, imgElement) {
+        try {
+            const response = await fetch('http://localhost:5000/get_2d_structure', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ smiles: smiles, filename: 'input_structure.pdb' })
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.blob();
-                }
-                throw new Error('Network response was not ok.');
-            })
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-                inputDownloadLink.href = url;
-            })
-            .catch(error => {
-                console.error('Error:', error);
+                body: JSON.stringify({ smiles })
             });
-        };
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-
-    // Predict fragment
-    fetch('http://localhost:5000/predict_fragment', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ smiles: smiles })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) {
-            alert(data.error);
-            return;
-        }
-
-        // Display 2D image for fragmented molecule
-        fetch('http://localhost:5000/get_2d_structure', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ smiles: data.fragment_smiles })
-        })
-        .then(response => response.blob())
-        .then(blob => {
+            const blob = await response.blob();
             const url = window.URL.createObjectURL(blob);
-            document.getElementById('fragment-2d').src = url;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            imgElement.src = url;
+        } catch (error) {
+            console.error('Error fetching 2D structure:', error);
+        }
+    }
 
-        // Display 3D structure for fragmented molecule
-        const viewerFragmented = $3Dmol.createViewer("viewer-fragmented", {
-            defaultcolors: $3Dmol.rasmolElementColors,
-            backgroundColor: 'black'
-        });
-
-        viewerFragmented.addModel(data.pdb, "pdb");
-        viewerFragmented.setStyle({}, {stick: {colorscheme: 'Jmol'}});
-        viewerFragmented.zoomTo();
-        viewerFragmented.render();
-
-        const fragmentDownloadLink = document.getElementById('fragment-download');
-        fragmentDownloadLink.style.display = 'block';
-        fragmentDownloadLink.onclick = function() {
-            fetch('http://localhost:5000/download_pdb', {
+    async function fetch3DStructure(smiles, viewerElement, downloadElement, filename) {
+        try {
+            const response = await fetch('http://localhost:5000/get_3d_structure', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ smiles: data.fragment_smiles, filename: 'fragment_structure.pdb' })
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.blob();
-                }
-                throw new Error('Network response was not ok.');
-            })
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-                fragmentDownloadLink.href = url;
-            })
-            .catch(error => {
-                console.error('Error:', error);
+                body: JSON.stringify({ smiles })
             });
-        };
+            const data = await response.json();
 
-        // Display fragment properties in a formatted manner
-        const propertiesDiv = document.getElementById('fragment-properties');
-        propertiesDiv.innerHTML = `
-            <h5 class="pink-title">Properties:</h5>
-            <p></p>
-            <p><strong>SMILES:</strong> ${data.fragment_smiles}</p>
-            <p><strong>Molecular Weight:</strong> ${data.properties.molecular_weight} Da</p>
-            <p><strong>LogP Value:</strong> ${data.properties.log_p}</p>
-            <p><strong>Hydrogen Bond Acceptors:</strong> ${data.properties.hydrogen_bond_acceptors}</p>
-            <p><strong>Hydrogen Bond Donors:</strong> ${data.properties.hydrogen_bond_donors}</p>
-            <p><strong>Topological Polar Surface Area:</strong> ${data.properties.tpsa} Å²</p>
-        `;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+            const viewerInstance = $3Dmol.createViewer(viewerElement, {
+                defaultcolors: $3Dmol.rasmolElementColors,
+                backgroundColor: 'black'
+            });
 
-// Toggle functionality
-document.getElementById('toggle-input-view').addEventListener('click', function() {
-    const viewer = document.getElementById('viewer');
-    const img2d = document.getElementById('input-2d');
-    const is2d = img2d.style.display === 'block';
+            viewerInstance.addModel(data.pdb, "pdb");
+            viewerInstance.setStyle({}, {stick: {colorscheme: 'Jmol'}});
+            viewerInstance.zoomTo();
+            viewerInstance.render();
 
-    if (is2d) {
-        img2d.style.display = 'none';
-        viewer.style.display = 'block';
-        this.textContent = 'Show 2D View';
-    } else {
-        img2d.style.display = 'block';
-        viewer.style.display = 'none';
-        this.textContent = 'Show 3D View';
+            downloadElement.style.display = 'block';
+            downloadElement.onclick = async function() {
+                try {
+                    const response = await fetch('http://localhost:5000/download_pdb', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ smiles, filename })
+                    });
+                    if (response.ok) {
+                        const blob = await response.blob();
+                        const url = window.URL.createObjectURL(blob);
+                        downloadElement.href = url;
+                    } else {
+                        throw new Error('Network response was not ok.');
+                    }
+                } catch (error) {
+                    console.error('Error downloading PDB:', error);
+                }
+            };
+        } catch (error) {
+            console.error('Error fetching 3D structure:', error);
+        }
     }
-});
 
-document.getElementById('toggle-fragment-view').addEventListener('click', function() {
-    const viewer = document.getElementById('viewer-fragmented');
-    const img2d = document.getElementById('fragment-2d');
-    const is2d = img2d.style.display === 'block';
+    async function predictFragment(smiles, suffix) {
+        try {
+            const response = await fetch('http://localhost:5000/predict_fragment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ smiles, protein })
+            });
+            const data = await response.json();
+            if (data.error) {
+                alert(data.error);
+                return null;
+            }
 
-    if (is2d) {
-        img2d.style.display = 'none';
-        viewer.style.display = 'block';
-        this.textContent = 'Show 2D View';
-    } else {
-        img2d.style.display = 'block';
-        viewer.style.display = 'none';
-        this.textContent = 'Show 3D View';
+            document.getElementById(`fragment-properties${suffix}`).innerHTML = `
+                <p><strong>SMILES:</strong> ${data.fragment_smiles}</p>
+                <p><strong>Molecular Weight:</strong> ${data.properties.molecular_weight} Da</p>
+                <p><strong>LogP Value:</strong> ${data.properties.log_p}</p>
+                <p><strong>Hydrogen Bond Acceptors:</strong> ${data.properties.hydrogen_bond_acceptors}</p>
+                <p><strong>Hydrogen Bond Donors:</strong> ${data.properties.hydrogen_bond_donors}</p>
+                <p><strong>Topological Polar Surface Area:</strong> ${data.properties.tpsa} Å²</p>
+            `;
+
+            await fetch2DStructure(data.fragment_smiles, document.getElementById(`fragment-2d${suffix}`));
+            await fetch3DStructure(data.fragment_smiles, document.getElementById(`viewer-fragmented${suffix === "1" ? "" : "2"}`), document.getElementById(`fragment-download${suffix}`), `fragment_structure${suffix}.pdb`);
+
+            return data.fragment_smiles;
+        } catch (error) {
+            console.error('Error predicting fragment:', error);
+            return null;
+        }
     }
+
+    async function combineFragments(smiles1, smiles2) {
+
+        try {
+            const response = await fetch('http://localhost:5000/combine', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ smiles1, smiles2 })
+            });
+            const data = await response.json();
+            const combinedContainer = document.getElementById('combined-container');
+            combinedContainer.innerHTML = ''; // Clear previous results
+            console.log(data.combined_smiles)
+            data.combined_smiles.forEach((fragment, index) => {
+                const combinedDiv = document.createElement('div');
+                combinedDiv.id = `combined-${index}`;
+                combinedDiv.innerHTML = `
+                    <h3>Combined Fragment ${index + 1}</h3>
+                    <button id="toggle-combined-view-${index}">Show 3D View</button>
+                    <img id="combined-2d-${index}" style="display:none;" />
+                    <div id="viewer-combined-${index}" style="width: 400px; height: 400px; display:none;"></div>
+                    <a id="combined-download-${index}" style="display:none;">Download 3D Structure</a>
+                    <div id="combined-properties-${index}">
+                        <p><strong>SMILES:</strong> ${fragment.smiles}</p>
+                        <p><strong>Molecular Weight:</strong> ${fragment.properties.molecular_weight} Da</p>
+                        <p><strong>LogP Value:</strong> ${fragment.properties.log_p}</p>
+                        <p><strong>Hydrogen Bond Acceptors:</strong> ${fragment.properties.hydrogen_bond_acceptors}</p>
+                        <p><strong>Hydrogen Bond Donors:</strong> ${fragment.properties.hydrogen_bond_donors}</p>
+                        <p><strong>Topological Polar Surface Area:</strong> ${fragment.properties.tpsa} Å²</p>
+                    </div>
+                `;
+
+                combinedContainer.appendChild(combinedDiv);
+
+                fetch2DStructure(fragment.smiles, document.getElementById(`combined-2d-${index}`));
+                fetch3DStructure(fragment.smiles, document.getElementById(`viewer-combined-${index}`), document.getElementById(`combined-download-${index}`), `combined_structure_${index}.pdb`);
+
+                document.getElementById(`toggle-combined-view-${index}`).addEventListener('click', function() {
+                    toggleCombinedView(index);
+                });
+            });
+        } catch (error) {
+            console.error('Error combining fragments:', error);
+        }
+    }
+
+    async function processCompounds() {
+        document.getElementById('subtitle-inputted1').style.display = 'block';
+        document.getElementById('toggle-input-view1').style.display = 'block';
+        document.getElementById('subtitle-fragmented1').style.display = 'block';
+        document.getElementById('toggle-fragment-view1').style.display = 'block';
+
+        document.getElementById('subtitle-inputted2').style.display = 'block';
+        document.getElementById('toggle-input-view2').style.display = 'block';
+        document.getElementById('subtitle-fragmented2').style.display = 'block';
+        document.getElementById('toggle-fragment-view2').style.display = 'block';
+
+        await fetch2DStructure(smiles1, document.getElementById('input-2d1'));
+        await fetch3DStructure(smiles1, document.getElementById('viewer'), document.getElementById('input-download1'), 'input_structure1.pdb');
+
+        await fetch2DStructure(smiles2, document.getElementById('input-2d2'));
+        await fetch3DStructure(smiles2, document.getElementById('viewer2'), document.getElementById('input-download2'), 'input_structure2.pdb');
+
+        const fragment1 = await predictFragment(smiles1, "1");
+        const fragment2 = await predictFragment(smiles2, "2");
+
+        if (fragment1 && fragment2) {
+            document.getElementById('subtitle-combined').style.display = 'block';
+            // document.getElementById('toggle-combined-view-${index}').style.display = 'block';
+            await combineFragments(fragment1, fragment2);
+        }
+    }
+
+    processCompounds();
 });
 
-function createSparkle(x, y) {
-    const sparkle = document.createElement('div');
-    sparkle.classList.add('sparkle');
-    sparkle.style.left = `${x}px`;
-    sparkle.style.top = `${y}px`;
-    document.body.appendChild(sparkle);
+function toggleInputView(suffix) {
+    const input2D = document.getElementById(`input-2d${suffix}`);
+    const viewer = document.getElementById(`viewer${suffix === "1" ? "" : "2"}`);
+    const button = document.getElementById(`toggle-input-view${suffix}`);
 
-    setTimeout(() => {
-        sparkle.remove();
-    }, 500); // Duration of the sparkle animation
+    if (input2D.style.display === 'none') {
+        input2D.style.display = 'block';
+        viewer.style.display = 'none';
+        button.textContent = 'Show 3D View';
+    } else {
+        input2D.style.display = 'none';
+        viewer.style.display = 'block';
+        button.textContent = 'Show 2D View';
+    }
 }
 
-// Add event listeners to all pink buttons
-document.querySelectorAll('.btn-pink').forEach(button => {
-    button.addEventListener('click', function(event) {
-        // Calculate the sparkle coordinates relative to the entire document
-        const x = event.pageX;
-        const y = event.pageY;
-        // Create the sparkle at the calculated coordinates
-        createSparkle(x, y);
-    });
+function toggleFragmentView(suffix) {
+    const fragment2D = document.getElementById(`fragment-2d${suffix}`);
+    const viewerFragmented = document.getElementById(`viewer-fragmented${suffix === "1" ? "" : "2"}`);
+    const button = document.getElementById(`toggle-fragment-view${suffix}`);
+
+    if (fragment2D.style.display === 'none') {
+        fragment2D.style.display = 'block';
+        viewerFragmented.style.display = 'none';
+        button.textContent = 'Show 3D View';
+    } else {
+        fragment2D.style.display = 'none';
+        viewerFragmented.style.display = 'block';
+        button.textContent = 'Show 2D View';
+    }
+}
+
+function toggleCombinedView() {
+    const combined2D = document.getElementById('combined-2d');
+    const viewerCombined = document.getElementById('viewer-combined');
+    const button = document.getElementById('toggle-combined-view');
+
+    if (combined2D.style.display === 'none') {
+        combined2D.style.display = 'block';
+        viewerCombined.style.display = 'none';
+        button.textContent = 'Show 3D View';
+    } else {
+        combined2D.style.display = 'none';
+        viewerCombined.style.display = 'block';
+        button.textContent = 'Show 2D View';
+    }
+}
+
+document.getElementById('toggle-input-view1').addEventListener('click', function() {
+    toggleInputView('1');
 });
+
+document.getElementById('toggle-input-view2').addEventListener('click', function() {
+    toggleInputView('2');
+});
+
+document.getElementById('toggle-fragment-view1').addEventListener('click', function() {
+    toggleFragmentView('1');
+});
+
+document.getElementById('toggle-fragment-view2').addEventListener('click', function() {
+    toggleFragmentView('2');
+});
+
+
